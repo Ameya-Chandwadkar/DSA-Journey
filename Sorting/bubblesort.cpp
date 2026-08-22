b002e76f5faece1484e441474aa80d2eed69aa7d
Ameya Chandwadkar <ameya.chandwadkar@gmail.com>
2026-08-22 11:05:32 +0530

    buuble sort changed

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -1,34 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::swap
+
 using namespace std;
 
-int main(){
-    cout<<"Enter the number of values in the array:";
+int main() {
+    cout << "Enter the number of values in the array: ";
     int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int j=n-1;
-    while(j>=0){
-        int temp;
-        for(int i=0;i<=j-1;i++){
-            for(int k=i+1;k<=j;k++){
-                if(arr[i]>arr[k]){
-                    temp=arr[i];
-                    arr[i]=arr[k];
-                    arr[k]=temp;
-                    break;
-                }
+    cin >> n;
 
+    
+    vector<int> arr(n); 
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+   
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+               
+                swap(arr[j], arr[j + 1]);
             }
         }
+    }
 
-        j=j-1;
+    cout << "Sorted array:";
+    for (int i = 0; i < n; i++) {
+        cout << " " << arr[i];
     }
-    cout<<"Sorted array:";
-    for(int i=0;i<n;i++){
-        cout<<" "<<arr[i];
-    }
+    cout << endl;
+
     return 0;
 }
